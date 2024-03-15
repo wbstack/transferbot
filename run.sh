ebbae04ef5c95f828841f6c8e150479d9777da07
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2024-03-15 17:09:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,14 @@
 set -eu
 set -o pipefail
 
-docker run --rm \
-    -e TARGET_WIKI_OAUTH_CONSUMER_TOKEN \
-    -e TARGET_WIKI_OAUTH_CONSUMER_SECRET \
-    -e TARGET_WIKI_OAUTH_ACCESS_TOKEN \
-    -e TARGET_WIKI_OAUTH_ACCESS_SECRET \
+docker run \
+    --rm \
+    --interactive \
+    --tty \
+    --env TARGET_WIKI_OAUTH_CONSUMER_TOKEN \
+    --env TARGET_WIKI_OAUTH_CONSUMER_SECRET \
+    --env TARGET_WIKI_OAUTH_ACCESS_TOKEN \
+    --env TARGET_WIKI_OAUTH_ACCESS_SECRET \
     ghcr.io/wbstack/transferbot:main \
     "$SOURCE_WIKI_URL" \
     "$TARGET_WIKI_URL" \
