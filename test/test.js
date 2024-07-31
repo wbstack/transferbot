3e8641f4e5d1b5296626c4b39d5626766b3fec15
Frederik Ring <frederik.ring@wikimedia.de>
2024-07-31 16:33:25 +0200

    feat: implement entity transfer

--- test/test.js
+++ test/test.js
@@ -1,8 +1,66 @@
 #!/usr/bin/env node
 
-const assert = require('assert')
-const test = require('node:test')
+import assert from 'assert'
+import test from 'node:test'
 
-test('setting this up', t => {
-	assert.ok(true)
+import { pickKeys, pickLanguages } from './../lib/util.js'
+
+test('util.pickKeys', async t => {
+    await t.test('empty', t => {
+        assert.deepStrictEqual(pickKeys({}), {})
+    })
+    await t.test('picks keys', t => {
+        const result = pickKeys({
+            foo: 'bar',
+            baz: {
+                foo: 'bar',
+            },
+            qux: 12
+        }, 'baz', 'qux')
+
+        assert.deepStrictEqual(result, {
+            baz: {
+                foo: 'bar'
+            },
+            qux: 12
+        })
+    })
+})
+
+test('util.pickLanguages', async t => {
+    await t.test('empty', t => {
+        assert.deepStrictEqual(pickLanguages({}), {})
+    })
+
+    await t.test('skip languages', t => {
+        const result = pickLanguages({
+            labels: {
+                en: {
+                    language: 'en',
+                    value: 'pipe',
+                },
+                fr: {
+                    language: 'fr',
+                    value: 'pipe',
+                },
+                de: {
+                    language: 'de',
+                    value: 'Pfeife',
+                }
+            }
+        }, 'en', 'fr')
+
+        assert.deepStrictEqual(result, {
+            labels: {
+                en: {
+                    language: 'en',
+                    value: 'pipe',
+                },
+                fr: {
+                    language: 'fr',
+                    value: 'pipe',
+                }
+            }
+        })
+    })
 })
