5b368bb93c292c61f3d1580a12d270e4b1ffb676
Frederik Ring <frederik.ring@wikimedia.de>
2024-08-01 09:37:35 +0200

    chore: cleanup

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,34 +3,34 @@
 import WikibaseRepo from './../lib/repo.js'
 import { pickLanguages, pickKeys } from './../lib/util.js'
 
-void (async() => {
-    const [source, target, ...entities] = process.argv.slice(2)
-    const sourceRepo = new WikibaseRepo(source)
-    const targetRepo = new WikibaseRepo(target, {
-        oauth: {
-          consumer_key: process.env.TARGET_WIKI_OAUTH_CONSUMER_TOKEN,
-          consumer_secret: process.env.TARGET_WIKI_OAUTH_CONSUMER_SECRET,
-          token: process.env.TARGET_WIKI_OAUTH_ACCESS_TOKEN,
-          token_secret: process.env.TARGET_WIKI_OAUTH_ACCESS_SECRET
-        }
-    })
+;(async () => {
+  const [source, target, ...entities] = process.argv.slice(2)
+  const sourceRepo = new WikibaseRepo(source)
+  const targetRepo = new WikibaseRepo(target, {
+    oauth: {
+      consumer_key: process.env.TARGET_WIKI_OAUTH_CONSUMER_TOKEN,
+      consumer_secret: process.env.TARGET_WIKI_OAUTH_CONSUMER_SECRET,
+      token: process.env.TARGET_WIKI_OAUTH_ACCESS_TOKEN,
+      token_secret: process.env.TARGET_WIKI_OAUTH_ACCESS_SECRET
+    }
+  })
 
-    const contentLanguages = await targetRepo.getContentLanguages()
+  const contentLanguages = await targetRepo.getContentLanguages()
 
-    let data = await sourceRepo.getEntities(...entities)
-    data = data
-        .map(e => pickKeys(e, 'type', 'labels', 'descriptions', 'aliases', 'datatype'))
-        .map(e => pickLanguages(e, ...contentLanguages))
+  let data = await sourceRepo.getEntities(...entities)
+  data = data
+    .map(e => pickKeys(e, 'type', 'labels', 'descriptions', 'aliases', 'datatype'))
+    .map(e => pickLanguages(e, ...contentLanguages))
 
-    await targetRepo.createEntities(...data)
-    return `Sucessfully transferred ${entities.length} entities from ${source} to ${target}.`
+  await targetRepo.createEntities(...data)
+  return `Sucessfully transferred ${entities.length} entities from ${source} to ${target}.`
 })()
-    .then((result) => {
-        if (result) {
-            console.log(result)
-        }
-    })
-    .catch((err) => {
-        console.error(err)
-        process.exit(1)
-    })
+  .then((result) => {
+    if (result) {
+      console.log(result)
+    }
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
--- test/test.js
+++ test/test.js
@@ -1,66 +1,66 @@
 #!/usr/bin/env node
 
-import assert from 'assert'
+import assert from 'node:assert'
 import test from 'node:test'
 
 import { pickKeys, pickLanguages } from './../lib/util.js'
 
 test('util.pickKeys', async t => {
-    await t.test('empty', t => {
-        assert.deepStrictEqual(pickKeys({}), {})
-    })
-    await t.test('picks keys', t => {
-        const result = pickKeys({
-            foo: 'bar',
-            baz: {
-                foo: 'bar',
-            },
-            qux: 12
-        }, 'baz', 'qux')
+  await t.test('empty', t => {
+    assert.deepStrictEqual(pickKeys({}), {})
+  })
+  await t.test('picks keys', t => {
+    const result = pickKeys({
+      foo: 'bar',
+      baz: {
+        foo: 'bar'
+      },
+      qux: 12
+    }, 'baz', 'qux')
 
-        assert.deepStrictEqual(result, {
-            baz: {
-                foo: 'bar'
-            },
-            qux: 12
-        })
+    assert.deepStrictEqual(result, {
+      baz: {
+        foo: 'bar'
+      },
+      qux: 12
     })
+  })
 })
 
 test('util.pickLanguages', async t => {
-    await t.test('empty', t => {
-        assert.deepStrictEqual(pickLanguages({}), {})
-    })
+  await t.test('empty', t => {
+    assert.deepStrictEqual(pickLanguages({}), {})
+  })
 
-    await t.test('skip languages', t => {
-        const result = pickLanguages({
-            labels: {
-                en: {
-                    language: 'en',
-                    value: 'pipe',
-                },
-                fr: {
-                    language: 'fr',
-                    value: 'pipe',
-                },
-                de: {
-                    language: 'de',
-                    value: 'Pfeife',
-                }
-            }
-        }, 'en', 'fr')
+  await t.test('skip languages', t => {
+    const result = pickLanguages({
+      labels: {
+        en: {
+          language: 'en',
+          value: 'pipe'
+        },
+        fr: {
+          language: 'fr',
+          value: 'pipe'
+        },
+        de: {
+          language: 'de',
+          value: 'Pfeife'
+        }
+      }
+    }, 'en', 'fr')
 
-        assert.deepStrictEqual(result, {
-            labels: {
-                en: {
-                    language: 'en',
-                    value: 'pipe',
-                },
-                fr: {
-                    language: 'fr',
-                    value: 'pipe',
-                }
-            }
-        })
+    assert.deepStrictEqual(result, {
+      labels: {
+        en: {
+          language: 'en',
+          value: 'pipe'
+        },
+        fr: {
+          language: 'fr',
+          value: 'pipe'
+        }
+      }
     })
+  })
 })
